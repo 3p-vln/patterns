6c3213fd4e1cfb98753c342dd1838e716a38bfcb
3p-vln <vldknovak@gmail.com>
2024-07-20 02:11:03 +0300

    fix abstract factory

--- dist/abstract factory/abstract-factory.js
+++ dist/abstract factory/abstract-factory.js
@@ -1,5 +1,4 @@
 "use strict";
-// Concrete Products for Email
 class EmailTextMessage {
     sendMessage(content, recipient) {
         console.log(`Sending email to ${recipient}: ${content}`);
@@ -10,7 +9,6 @@ class EmailMediaMessage {
         console.log(`Sending email with media to ${recipient}: ${content}, Media URL: ${mediaUrl}`);
     }
 }
-// Concrete Products for SMS
 class SmsTextMessage {
     sendMessage(content, recipient) {
         console.log(`Sending SMS to ${recipient}: ${content}`);
@@ -21,18 +19,6 @@ class SmsMediaMessage {
         console.log(`Sending SMS with media to ${recipient}: ${content}, Media URL: ${mediaUrl}`);
     }
 }
-// Concrete Products for Push Notifications
-class PushTextMessage {
-    sendMessage(content, recipient) {
-        console.log(`Sending push notification to ${recipient}: ${content}`);
-    }
-}
-class PushMediaMessage {
-    sendMessage(content, recipient, mediaUrl) {
-        console.log(`Sending push notification with media to ${recipient}: ${content}, Media URL: ${mediaUrl}`);
-    }
-}
-// Concrete Factories
 class EmailMessageAbstractFactory {
     createTextMessage() {
         return new EmailTextMessage();
@@ -49,27 +35,15 @@ class SmsMessageAbstractFactory {
         return new SmsMediaMessage();
     }
 }
-class PushMessageAbstractFactory {
-    createTextMessage() {
-        return new PushTextMessage();
-    }
-    createMediaMessage() {
-        return new PushMediaMessage();
-    }
-}
-// Client Code
 function clientCode(factory) {
     const textMessage = factory.createTextMessage();
     const mediaMessage = factory.createMediaMessage();
     textMessage.sendMessage("Hello World!", "recipient@example.com");
     mediaMessage.sendMessage("Hello World with Media!", "recipient@example.com", "http://example.com/media.jpg");
 }
-// Testing
 console.log("Client: Testing client code with EmailMessageAFactory...");
 clientCode(new EmailMessageAbstractFactory());
 console.log("");
 console.log("Client: Testing client code with SmsMessageFactory...");
 clientCode(new SmsMessageAbstractFactory());
 console.log("");
-console.log("Client: Testing client code with PushMessageFactory...");
-clientCode(new PushMessageAbstractFactory());
